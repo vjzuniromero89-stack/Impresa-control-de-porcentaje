f7e2d02a60eb67553cdb58b5c2df45825166497d
vjzuniromero89-stack <vjzuniromero89@gmail.com>
2026-09-16 12:59:44 -0400

    Delete layout.js

--- layout.js
+++
@@ -1,3 +0,0 @@
-import './globals.css';
-export const metadata={title:'Sociedad 50/50',description:'Control de deudas y participación'};
-export default function Layout({children}){return <html lang="es"><body>{children}</body></html>}
